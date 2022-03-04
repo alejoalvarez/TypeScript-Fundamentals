9960f3e805e17ea5e93a7415605c4962b1ecdab6
Alejandro Vargas Alvarez <alejo.alvarezv@gmail.com>
2022-03-03 19:31:23 -0500

    Optional parametes in interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -32,3 +32,41 @@ showPicture({
     orientation: PhotoOrientation.Portrait,
     // extra: 'test'// Error
 });
+
+//Optional parameters
+interface PictureConfig{
+    title?: string;
+    date?: string;
+    orientation?: PhotoOrientation
+}
+
+function generatePicture(config: PictureConfig){
+    const pic = {title: 'Default', date: '2020-03'};
+    if(config.title){
+        pic.title = config.title;
+        if (config.date){
+    }
+        pic.date = config.date;
+    }
+    return pic;
+}
+
+let picture = generatePicture({});
+console.log(">>picture:", picture);
+
+// Interface User
+interface User{
+    readonly id: number;
+    username: string;
+    isPro: boolean;
+}
+
+let user: User;
+user = {id: 10, username: 'Alejo', isPro: true}
+console.log('>>user:', user);
+user.username = 'Alejo2';
+//user.id = 20; // Error
+console.log('>>user:', user);
+
+
+
